20884992ec2898c871eb7f69a70205542a17aa76
Heeuul <aimanhans@yahoo.com>
2023-05-31 22:31:34 +0800

    310523 - 22:31 - Realtime Onboard Addition

--- components/Onboard.js
+++ components/Onboard.js
@@ -5,21 +5,27 @@ import { OnboardFlow } from "react-native-onboard";
 const onboardPages = [
   {
     title: "Hello!",
-    subtitle: "Welcome to the Crypto Price Tracker",
+    subtitle: "Welcome to the\nCrypto Price Tracker",
     imageUri:
       "https://raw.githubusercontent.com/Heeuul/CryptoPriceTracker/main/assets/Onboard1.png?token=GHSAT0AAAAAACDKVIMQL5INSHIHTMXTCJNSZDXK4BA",
   },
   {
-    title: "Real time updates",
-    subtitle: "Compare prices between currencies",
+    title: "Real Time Updates",
+    subtitle: "Get the latest info of the prices",
+    imageUri:
+      "https://www.shutterstock.com/image-vector/real-time-icon-solid-260nw-1722565684.jpg",
+  },
+  {
+    title: "Multiple Currencies",
+    subtitle: "Compare prices\nbetween currencies",
     imageUri:
       "https://raw.githubusercontent.com/Heeuul/CryptoPriceTracker/main/assets/Onboard2.png?token=GHSAT0AAAAAACDKVIMQOI34R57CDJZNLPRGZDXK4XA",
   },
   {
-    title: "View trends of the past week",
-    subtitle: "Press on a currency to view chart",
+    title: "Past Week's Trend",
+    subtitle: "Tap on a currency to\nview changes for\nthe past 7 days",
     imageUri:
-      "https://raw.githubusercontent.com/Heeuul/CryptoPriceTracker/main/assets/Onboard3.png?token=GHSAT0AAAAAACDKVIMR2FDGRU73INALTFECZDXK4ZA",
+      "https://raw.githubusercontent.com/Heeuul/CryptoPriceTracker/main/assets/Onboard3.png?token=GHSAT0AAAAAACDKVIMRJQXUPPAWBD4UCZO4ZDXLCSQ",
   },
 ];
 
